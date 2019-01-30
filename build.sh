63b700b99f4b56385b5c1adeb90e16b741779bf6
Philipp Klaus <philipp.l.klaus@web.de>
2019-01-30 14:35:02 +0100

    tag as pklaus/root_opencascade

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ OCC_VERSION=6.9.1
 ROOT_VERSION=6.12.06
 
 docker build \
-  -t pklaus/root:v${ROOT_VERSION}_${OCC_VERSION} \
+  -t pklaus/root_opencascade:v${ROOT_VERSION}_${OCC_VERSION} \
   --build-arg JOBS=$JOBS \
   --build-arg OCC_VERSION=$OCC_VERSION \
   --build-arg ROOT_VERSION=$ROOT_VERSION \
